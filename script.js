2d7952bba845728e2774737dd3a89d33c208b49b
Vaghela divyesh <86907707+iamdivyeshh@users.noreply.github.com>
2024-01-14 17:12:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ document.querySelector(".scroll-btn").addEventListener("click", () => {
     document.querySelector("html").style.scrollBehavior = "unset";
   }, 1000);
 });
+document.querySelector(".sidebar").addEventListener("click", () => {
+  document.querySelector("html").style.scrollBehavior = "smooth";
+  setTimeout(() => {
+    document.querySelector("html").style.scrollBehavior = "unset";
+  }, 1000);
+});
